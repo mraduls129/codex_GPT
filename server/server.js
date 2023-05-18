a46727f6fee2a935540386c532fa1ae6f9700638
MRADUL SHARMA <129748782+mraduls129@users.noreply.github.com>
2023-05-18 15:32:07 +0530

    Update server.js
    i thing that domain is not working here

--- server/server.js
+++ server/server.js
@@ -46,4 +46,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on http://localhost:3000'))
