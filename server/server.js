58ab4a3972d9d4829c8cb452dc6cfa11c6971cbb
MRADUL SHARMA <129748782+mraduls129@users.noreply.github.com>
2023-05-18 19:27:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -46,4 +46,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:3000'))
+app.listen(3000, () => console.log('AI server started on http://localhost:3000'))
